da1357c044b7f20629435c016abc6810e7bc9ca6
darya <edinadk@mer.ci.nsu.ru>
2022-04-15 18:01:11 +0700

    done!!

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const API_KEY = "lyLn4WTM2Fzl6xjZKy08Te4fcTcRf2KB";
-const URL = `http://api.giphy.com/v1/gifs/search?api_key=${API_KEY}`;
+const SEARCH = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=50&q=`;
+const API_URL_POPULAR = `http://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=50`;
 
 let line = "найти гифку";
 let speed = 100;
@@ -23,6 +24,55 @@ input.addEventListener('focus', () => {
     input.value="";
 });
 
-function getData() {
-    fetch(URL);
+function _Fetch(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(content => {
+            for (let i = 0; i < 50; i++) {
+                console.log(content.data);
+                console.log("META", content.meta);
+                let fig = document.createElement("figure");
+                let img = document.createElement("img");
+                let fc = document.createElement("figcaption");
+                img.src = content.data[i].images.fixed_height.url;
+                img.alt = content.data[i].title;
+                fc.textContent = content.data[i].title;
+                fig.appendChild(img);
+                fig.appendChild(fc);
+                let out = document.querySelector(".giphs");
+                out.insertAdjacentElement("afterbegin", fig);
+                document.querySelector("#input").value = "";
+            }
+        })
+        .catch(err => {
+            console.error(err);
+        });
 }
+
+document.addEventListener("DOMContentLoaded", init);
+function init(url) {
+    document.getElementById("searchGiphButton").addEventListener("click", ev => {
+        ev.preventDefault();
+        if (input.value === '') {
+            alert("введите запрос");
+        } else {
+            let str = document.getElementById("input").value.trim();
+            url = url.concat(str);
+            console.log(url);
+            document.querySelector(".giphs").innerHTML = "";
+            _Fetch(url);
+        }
+    });
+}
+
+function showTrend(url)
+{
+    console.log(url);
+    _Fetch(url);
+}
+
+showTrend(API_URL_POPULAR);
+init(SEARCH);
+
+
+
